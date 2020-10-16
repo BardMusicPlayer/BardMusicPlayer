c95904afa1baf41fc1d0f769258b635ff350d093
Dustin Hopper <hopper.dustin@gmail.com>
2020-10-16 03:00:59 -0600

    Main: Fix looping for local perf, ensure it works for solo
    This removes the looping logic itself from BmpPlayer and moves it into
    BmpMain. Since LocalOrchestra uses separate, non-binding sequences, this
    is required in order for looping to work with LocalOrchestra.

--- FFBardMusicPlayer/Forms/BmpMain.cs
+++ FFBardMusicPlayer/Forms/BmpMain.cs
@@ -558,10 +558,6 @@ namespace FFBardMusicPlayer.Forms {
 				if (proceedPlaylistMidi)
                 {
 					Player.Player.Stop();
-                    if (LocalOrchestra.OrchestraEnabled)
-                    {
-                        LocalOrchestra.PerformerStop();
-                    }
 				}
 			}
 		}
@@ -602,7 +598,17 @@ namespace FFBardMusicPlayer.Forms {
 		}
 
 		private void OnPlayStatusEnded(object o, EventArgs e) {
-			if(!Player.Loop) {
+            if (LocalOrchestra.OrchestraEnabled)
+            {
+                LocalOrchestra.PerformerStop();
+            }
+            if (Player.Loop)
+            {
+                Player.Player.Play();
+                if (LocalOrchestra.OrchestraEnabled) LocalOrchestra.PerformerPlay(true);
+            }
+            else
+            {
 				proceedPlaylistMidi = true;
 				this.NextSong();
 			}
