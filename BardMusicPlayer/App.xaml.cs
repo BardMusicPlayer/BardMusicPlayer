5bf4c5568d70449de557894338411138ba507e99
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-04-22 02:17:47 -0400

    Explicitly load dlls from Ui's output path in Debug/Release to match how Publish loads dlls.

--- BardMusicPlayer/App.xaml.cs
+++ BardMusicPlayer/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
 
 namespace BardMusicPlayer
 {
@@ -10,13 +13,29 @@ namespace BardMusicPlayer
         {
 
 #if LOCAL
+            
+            foreach (string dll in Directory.EnumerateFiles(@".\","*.dll")) File.Delete(dll);
+#if DEBUG
+            var dlls = Directory.GetFiles(@"..\..\..\..\BardMusicPlayer.Ui\bin\Debug\net48", "*.dll");
+#else
+            var dlls = Directory.GetFiles(@"..\..\..\..\BardMusicPlayer.Ui\bin\Release\net48", "*.dll");
+#endif
+            Type viewsType = null;
+            foreach (var dll in dlls)
+            {
+                if (Path.GetFileName(dll).Equals("BardMusicPlayer.Ui.dll")) viewsType = Assembly.LoadFrom(dll).GetType("BardMusicPlayer.Ui.Main");
+                else Assembly.LoadFrom(dll);
+            }
+            dynamic main = Activator.CreateInstance(viewsType ?? throw new InvalidOperationException("BardMusicPlayer.Ui.dll"));
+            main.ShowDialog();
+            base.OnStartup(eventArgs);
 
 #else
 
+
+
+
 #endif
-
-
-
         }
     }
 }
