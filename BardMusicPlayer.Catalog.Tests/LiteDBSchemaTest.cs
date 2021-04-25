b60fd81d6fedd1fdcffea4daea1e55356d5da5b8
isaki <10616227+isaki@users.noreply.github.com>
2021-04-24 22:04:21 -0400

    Added unit test project for the catalog with one basic test

---
+++ BardMusicPlayer.Catalog.Tests/LiteDBSchemaTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BardMusicPlayer.Catalog.Tests
+{
+    [TestClass]
+    public class LiteDBSchemaTest
+    {
+        [TestMethod]
+        public void TestBasics()
+        {
+            int testId = 100;
+            byte testVersion = 40;
+
+            LiteDBSchema test = new LiteDBSchema()
+            {
+                Id = testId,
+                Version = testVersion
+            };
+
+            Assert.AreEqual(testId, test.Id);
+            Assert.AreEqual(testVersion, test.Version);
+        }
+    }
+}
