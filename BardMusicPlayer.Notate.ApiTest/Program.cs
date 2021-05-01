dc03d2c4799766a9c0a2fd61ce195f39243afeca
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-05-01 13:14:53 -0400

    upgrade newtonsoft, remove uneeded firewall helper in doot, create api test program for upcoming notate changes

---
+++ BardMusicPlayer.Notate.ApiTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BardMusicPlayer.Notate.Processor;
+
+namespace BardMusicPlayer.Notate.ApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+         //   var processor = new ClassicProcessor();
+        }
+    }
+}
