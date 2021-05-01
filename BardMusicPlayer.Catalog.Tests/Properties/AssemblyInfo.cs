91e9aa4341a0ea80c8c5d7e090fa064d9220d40d
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-04-30 22:09:08 -0400

    Migrate to vs 2019 SDK style project

--- BardMusicPlayer.Catalog.Tests/Properties/AssemblyInfo.cs
+++ BardMusicPlayer.Catalog.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("BardMusicPlayer.Catalog.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BardMusicPlayer.Catalog.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("bb7b97af-912b-406a-b392-9405452e2253")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
