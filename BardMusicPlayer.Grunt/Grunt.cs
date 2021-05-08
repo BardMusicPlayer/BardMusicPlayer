9bd9ad7046a28d881ae8b249e5594ab6b1f14418
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-05-08 01:36:09 -0400

    Create Grunt project

---
+++ BardMusicPlayer.Grunt/Grunt.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * Copyright(c) 2021 MoogleTroupe, 2018-2020 parulina
+ * Licensed under the GPL v3 license. See https://github.com/BardMusicPlayer/BardMusicPlayer/blob/develop/LICENSE for full license information.
+ */
+
+namespace BardMusicPlayer.Grunt
+{
+    public class Grunt
+    {
+    }
+}
