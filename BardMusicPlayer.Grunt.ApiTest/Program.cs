5b46b669f7cdd52b5e3fa996ef0cd3c0fa2b928b
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-05-09 22:51:43 -0400

    Create grunt project and add dalamud plugin calls.

---
+++ BardMusicPlayer.Grunt.ApiTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BardMusicPlayer.Grunt.ApiTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Grunt.Instance.Start();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
