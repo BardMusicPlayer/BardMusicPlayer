b5eb4230274825dd4eb17950d8f9450782ce9ec7
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-05-11 02:33:08 -0400

    Cleanup warnings in Seer

--- BardMusicPlayer.Seer/Reader/Backend/Sharlayan/UnsafeNativeMethods.cs
+++ BardMusicPlayer.Seer/Reader/Backend/Sharlayan/UnsafeNativeMethods.cs
@@ -5,8 +5,6 @@
 
 using System;
 using System.Runtime.InteropServices;
-using System.Text;
-// ReSharper disable InconsistentNaming
 
 namespace BardMusicPlayer.Seer.Reader.Backend.Sharlayan
 {
@@ -16,36 +14,6 @@ namespace BardMusicPlayer.Seer.Reader.Backend.Sharlayan
 		{
 			PROCESS_VM_ALL = 2035711,
 			PROCESS_VM_READ = 0x10
-		}
-
-		public struct MEMORY_BASIC_INFORMATION
-		{
-			public IntPtr BaseAddress;
-
-			public IntPtr AllocationBase;
-
-			public uint AllocationProtect;
-
-			public IntPtr RegionSize;
-
-			public uint State;
-
-			public uint Protect;
-
-			public uint Type;
-
-			public override string ToString()
-			{
-				var stringBuilder = new StringBuilder();
-				stringBuilder.AppendFormat($"BaseAddress:{BaseAddress}{Environment.NewLine}");
-				stringBuilder.AppendFormat($"AllocationBase:{AllocationBase}{Environment.NewLine}");
-				stringBuilder.AppendFormat($"AllocationProtect:{AllocationProtect}{Environment.NewLine}");
-				stringBuilder.AppendFormat($"RegionSize:{RegionSize}{Environment.NewLine}");
-				stringBuilder.AppendFormat($"State:{State}{Environment.NewLine}");
-				stringBuilder.AppendFormat($"Protect:{Protect}{Environment.NewLine}");
-				stringBuilder.AppendFormat($"Type:{Type}{Environment.NewLine}");
-				return stringBuilder.ToString();
-			}
 		}
 
 		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
@@ -59,8 +27,5 @@ namespace BardMusicPlayer.Seer.Reader.Backend.Sharlayan
 
 		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
 		public static extern bool ReadProcessMemory(IntPtr processHandle, IntPtr lpBaseAddress, [In][Out] IntPtr lpBuffer, IntPtr regionSize, out IntPtr lpNumberOfBytesRead);
-
-		[DllImport("kernel32.dll")]
-		public static extern int VirtualQueryEx(IntPtr processHandle, IntPtr lpBaseAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
 	}
 }
