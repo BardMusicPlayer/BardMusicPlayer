d642c0f552f17b88bedb8210f666b54402043d72
Dustin Hopper <hopper.dustin@gmail.com>
2021-05-12 13:17:11 -0600

    Updater: Hook parsing of current and latest version, update UI to show

--- BardMusicPlayer.Updater/MainWindow.xaml.cs
+++ BardMusicPlayer.Updater/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace BardMusicPlayer.Updater
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Util.BmpVersion LocalVersion;
         private Dictionary<string, Util.BmpVersion> RemoteVersions;
 
         public MainWindow()
@@ -26,9 +27,18 @@ namespace BardMusicPlayer.Updater
             InitializeComponent();
         }
 
-        internal void ProvideVersions(Dictionary<string, Util.BmpVersion> inputRead)
+        internal void ProvideVersions(Util.BmpVersion localVersion, Dictionary<string, Util.BmpVersion> inputRead)
         {
-            this.RemoteVersions = inputRead.OrderBy(version => version.Value.beta).ThenByDescending(version => version.Value.build) as Dictionary<string, Util.BmpVersion>;
+            this.LocalVersion = localVersion;
+            this.RemoteVersions =
+                inputRead.OrderBy(version => version.Value.beta)
+                         .ThenByDescending(version => version.Value.build)
+                         .ToDictionary<KeyValuePair<string, Util.BmpVersion>, string, Util.BmpVersion>(pair => pair.Key, pair => pair.Value);
+
+            this.label_CurrentVersion.Content = "Current version: " + localVersion.build;
+            this.label_NewVersionAvailable.Content = "BMP version " + this.RemoteVersions.First().Value.build + " is available for download.";
+
+            this.tbox_PatchNotes.Text = this.RemoteVersions.Select(version => "website url: " + version.Key + @"/" + Environment.NewLine + "beta: " + version.Value.beta + Environment.NewLine + "commit: " + version.Value.commit + Environment.NewLine + "build: " + version.Value.build + Environment.NewLine).First();
         }
     }
 }
