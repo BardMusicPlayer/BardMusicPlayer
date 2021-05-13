96a2c50565994d52f03bfcc78f89f85bc0419457
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-05-13 03:26:40 -0400

    fix siren resource file

--- BardMusicPlayer.Siren/Properties/Resources.Designer.cs
+++ BardMusicPlayer.Siren/Properties/Resources.Designer.cs
@@ -36,7 +36,7 @@ namespace BardMusicPlayer.Siren.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BardMusicPlayer.Synth.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BardMusicPlayer.Siren.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
