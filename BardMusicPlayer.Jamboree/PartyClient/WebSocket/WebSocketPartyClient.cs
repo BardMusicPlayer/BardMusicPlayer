cd831adaf46cb3662a8ea114bdd6bbc12920326f
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-05-12 23:01:35 -0400

    create an interface

---
+++ BardMusicPlayer.Jamboree/PartyClient/WebSocket/WebSocketPartyClient.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Copyright(c) 2021 MoogleTroupe, 2018-2020 parulina
+ * Licensed under the GPL v3 license. See https://github.com/BardMusicPlayer/BardMusicPlayer/blob/develop/LICENSE for full license information.
+ */
+
+using System.Collections.Generic;
+
+namespace BardMusicPlayer.Jamboree.PartyClient.WebSocket
+{
+    internal class WebSocketPartyClient : IPartyClient
+    {
+        internal WebSocketPartyClient()
+        {
+        }
+
+        public string RequestParty()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool JoinParty(string party)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<string> RequestPartyMembers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public event IPartyClient.PartyMembersChangedHandler PartyMembersChanged;
+
+        public bool AssignPartyLeader(string partyMember)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string RequestPartyLeader()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public event IPartyClient.PartyLeaderChangedHandler PartyLeaderChanged;
+
+        public List<PartyGame> RequestPartyGames(string partyMember)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public event IPartyClient.PartyGamesChangedHandler PartyGamesChanged;
+
+        public void Dispose()
+        {
+        }
+    }
+}
