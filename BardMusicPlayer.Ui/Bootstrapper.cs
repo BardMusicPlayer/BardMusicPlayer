cff87529990f1a5d0e3a212954bce625d805874a
sabihoshi <sabihoshi.dev@gmail.com>
2021-05-25 10:09:50 +0800

    Move initialization to Bootstrapper

--- BardMusicPlayer.Ui/Bootstrapper.cs
+++ BardMusicPlayer.Ui/Bootstrapper.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 ﻿using System.Threading.Tasks;
 using System.Windows;
+using BardMusicPlayer.Coffer;
+using BardMusicPlayer.Grunt;
+using BardMusicPlayer.Pigeonhole;
+using BardMusicPlayer.Seer;
 using BardMusicPlayer.Ui.Utilities;
 using BardMusicPlayer.Ui.ViewModels;
 using Stylet;
@@ -12,6 +16,39 @@ namespace BardMusicPlayer.Ui
         private bool _started;
         private static readonly Lazy<Bootstrapper> LazyInstance = new(() => new Bootstrapper());
         public static Bootstrapper Instance => LazyInstance.Value;
+
+        protected override void OnStart()
+        {
+            BmpPigeonhole.Initialize(Globals.DataPath + @"\Configuration.json");
+
+            // var view = (MainView)View;
+            // LogManager.Initialize(new(view.Log));
+
+            BmpCoffer.Initialize(Globals.DataPath + @"\MusicCatalog.db");
+
+            BmpSeer.Instance.SetupFirewall("BardMusicPlayer");
+
+            BmpSeer.Instance.Start();
+
+            BmpGrunt.Instance.Start();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            LogManager.Shutdown();
+
+            // BmpMaestro.Instance.Stop();
+
+            BmpGrunt.Instance.Stop();
+
+            BmpSeer.Instance.Stop();
+
+            BmpSeer.Instance.DestroyFirewall("BardMusicPlayer");
+
+            BmpCoffer.Instance.Dispose();
+
+            BmpPigeonhole.Instance.Dispose();
+        }
 
         public Task<bool> StartUp(bool beta, int build, string commit, string exePath, string resourcePath,
             string dataPath, string[] args)
--- BardMusicPlayer.Ui/ViewModels/MainViewModel.cs
+++ BardMusicPlayer.Ui/ViewModels/MainViewModel.cs
@@ -1,15 +1,11 @@
-﻿/*
+/*
  * Copyright(c) 2021 MoogleTroupe, trotlinebeercan
  * Licensed under the GPL v3 license. See https://github.com/BardMusicPlayer/BardMusicPlayer/blob/develop/LICENSE for full license information.
  */
 
 using System.Windows;
 using System.Windows.Controls;
-using BardMusicPlayer.Coffer;
-using BardMusicPlayer.Grunt;
-using BardMusicPlayer.Pigeonhole;
-using BardMusicPlayer.Seer;
-using BardMusicPlayer.Ui.Utilities;
+using BardMusicPlayer.Ui.Notifications;
 using BardMusicPlayer.Ui.ViewModels.Settings;
 using Stylet;
 using StyletIoC;
@@ -36,39 +32,6 @@ namespace BardMusicPlayer.Ui.ViewModels
         public void Navigate(object sender, RoutedEventArgs e)
         {
             if (sender is Button { Tag: IScreen screen }) ActivateItem(screen);
-        }
-
-        protected override void OnViewLoaded()
-        {
-            BmpPigeonhole.Initialize(Globals.DataPath + @"\Configuration.json");
-
-            // var view = (MainView)View;
-            // LogManager.Initialize(new(view.Log));
-
-            BmpCoffer.Initialize(Globals.DataPath + @"\MusicCatalog.db");
-
-            BmpSeer.Instance.SetupFirewall("BardMusicPlayer");
-
-            BmpSeer.Instance.Start();
-
-            BmpGrunt.Instance.Start();
-        }
-
-        protected override void OnClose()
-        {
-            LogManager.Shutdown();
-
-            // BmpMaestro.Instance.Stop();
-
-            BmpGrunt.Instance.Stop();
-
-            BmpSeer.Instance.Stop();
-
-            BmpSeer.Instance.DestroyFirewall("BardMusicPlayer");
-
-            BmpCoffer.Instance.Dispose();
-
-            BmpPigeonhole.Instance.Dispose();
         }
     }
 }
