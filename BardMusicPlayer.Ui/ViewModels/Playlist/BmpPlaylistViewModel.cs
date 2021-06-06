b23e8f366f770e911dfd393d55b5739c151146e0
sabihoshi <sabihoshi.dev@gmail.com>
2021-06-07 07:10:14 +0800

    Playlist: Show playlists created

---
+++ BardMusicPlayer.Ui/ViewModels/Playlist/BmpPlaylistViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BardMusicPlayer.Coffer;
+using Stylet;
+
+namespace BardMusicPlayer.Ui.ViewModels.Playlist
+{
+    public class BmpPlaylistViewModel : Screen
+    {
+        private readonly IPlaylist _bmpPlaylist;
+        private bool _isReadOnly = true;
+
+        public BmpPlaylistViewModel(IPlaylist bmpPlaylist) { _bmpPlaylist = bmpPlaylist; }
+
+        public bool IsReadOnly
+        {
+            get => _isReadOnly;
+            set => SetAndNotify(ref _isReadOnly, value);
+        }
+
+        public string Name
+        {
+            get => _bmpPlaylist.GetName();
+            set
+            {
+                _bmpPlaylist.SetName(value);
+                IsReadOnly = true;
+            }
+        }
+
+        public void RenamePlaylist() { IsReadOnly = false; }
+    }
+}
