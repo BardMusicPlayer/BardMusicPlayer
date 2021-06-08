15661288691c79d5d6dfd06bb8dd1f1f5a959c4f
GiR-Zippo <GiR-Zippo@users.noreply.github.com>
2021-06-08 05:57:55 +0200

    Let the songlist do it's thing
    Signed-off-by: GiR_Zippo <none@local.host>

---
+++ BardMusicPlayer.Ui/ViewModels/Playlist/BmpSongViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BardMusicPlayer.Coffer;
+using Stylet;
+using System.Windows.Input;
+using BardMusicPlayer.Transmogrify.Song;
+
+namespace BardMusicPlayer.Ui.ViewModels.Playlist
+{
+    public class BmpSongViewModel : Screen
+    {
+        private readonly BmpSong _bmpSong;
+        private readonly PlaylistViewModel _playlistview;
+
+        public BmpSongViewModel(BmpSong bmpsong, PlaylistViewModel parent)
+        {
+            _bmpSong = bmpsong;
+            _playlistview = parent;
+            Title = _bmpSong.Title;
+        }
+
+
+        public string Title { get; set; }
+
+    }
+}
