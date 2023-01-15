1e0f58775ee584cfc67e2bd64641e285cc1839c2
Meowchestra <42126400+Meowchestra@users.noreply.github.com>
2023-01-15 06:27:43 -0800

    Remove Skinned UI (#187)

--- BardMusicPlayer/MainWindow.xaml.cs
+++ BardMusicPlayer/MainWindow.xaml.cs
@@ -1,27 +1,21 @@
-﻿using BardMusicPlayer.Ui.Skinned;
-using BardMusicPlayer.Ui.Classic;
+﻿using BardMusicPlayer.Ui.Classic;
 using System.Windows;
 using BardMusicPlayer.Pigeonhole;
-using System.Reflection;
 
 namespace BardMusicPlayer.Ui
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    /// Interaction Logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-
-            if (BmpPigeonhole.Instance.ClassicUi)
-                SwitchClassicStyle();
-            else
-                SwitchSkinnedStyle();
+            ClassicStyle();
         }
 
-        public void SwitchClassicStyle()
+        public void ClassicStyle()
         {
             this.Title = "BardMusicPlayer BETA Version: 2.X-CUSTOM";
             this.DataContext = new Classic_MainView();
@@ -30,15 +24,6 @@ namespace BardMusicPlayer.Ui
             this.Height = 665;
             this.Width = 855;
             this.ResizeMode = ResizeMode.CanResizeWithGrip;
-        }
-
-        public void SwitchSkinnedStyle()
-        {
-            this.DataContext = new Skinned_MainView();
-            this.AllowsTransparency = true;
-            this.Height = 174;
-            this.Width = 412;
-            this.ResizeMode = ResizeMode.NoResize;
         }
     }
 }
