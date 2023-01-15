0e32d3837ffb0aa0fa977130ca501a0fba506032
Meowchestra <42126400+Meowchestra@users.noreply.github.com>
2023-01-14 19:07:59 -0800

    Add KeyTranslation to Enums

---
+++ BardMusicPlayer.Quotidian/Enums/KeyTranslation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace BardMusicPlayer.Quotidian.Enums
+{
+    public class KeyTranslation
+    {
+        public static Dictionary<string, Keys> ASCIIToGame = new Dictionary<string, Keys>
+        {
+            { "1", Keys.D1 },
+            { "2", Keys.D2 },
+            { "3", Keys.D3 },
+            { "4", Keys.D4 },
+            { "5", Keys.D5 },
+            { "6", Keys.D6 },
+            { "7", Keys.D7 },
+            { "8", Keys.D8 },
+            { "9", Keys.D9 },
+            { "0", Keys.D0 },
+            { "A", Keys.A },
+            { "D", Keys.D },
+            { "E", Keys.E },
+            { "Q", Keys.Q },
+            { "S", Keys.S },
+            { "W", Keys.W },
+            { " ", Keys.Space }
+        };
+    }
+}
