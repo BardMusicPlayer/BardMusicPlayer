744277606277549a15245d67e4f7f8cce6a31cc2
Meowchestra <42126400+Meowchestra@users.noreply.github.com>
2023-01-14 16:10:39 -0800

    Updated Filefilters categories.
    * More granular selection so you can specifically sort for something if you have all files in a giant folder and only want to see a specific file type.

--- BardMusicPlayer/Globals/Globals.cs
+++ BardMusicPlayer/Globals/Globals.cs
@@ -9,7 +9,7 @@ namespace BardMusicPlayer.Ui.Globals
     public static class Globals
     {
         public static string DirectoryPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-        public static string FileFilters = "MIDI file|*.mid;*.midi;*.mmsong;*.mml";
+        public static string FileFilters = "All files|*.mid;*.midi;*.mmsong;*.mml;*.gp*|MMSong files|*.mmsong|MIDI files|*.mid;*.midi|MML files|*.mml|GP files|*.gp*";
         public static string MusicCatalogFilters = "Amp Catalog file|*.db";
         public static string DataPath;
         public enum Autostart_Types
