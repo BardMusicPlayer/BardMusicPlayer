d9d1a047612260bd6c98a997501bf73060ee698d
MoogleTroupe <69164391+troy-f@users.noreply.github.com>
2023-01-18 19:57:01 -0500

    remove unused autostart_types

--- BardMusicPlayer/Globals/Globals.cs
+++ BardMusicPlayer/Globals/Globals.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BardMusicPlayer.Ui.Globals
 {
@@ -12,11 +8,6 @@ namespace BardMusicPlayer.Ui.Globals
         public static string FileFilters = "All files|*.mid;*.midi;*.mmsong;*.mml;*.gp*|MIDI files|*.mid;*.midi|MMSong files|*.mmsong|MML files|*.mml|GP files|*.gp*";
         public static string MusicCatalogFilters = "Amp Catalog file|*.db";
         public static string DataPath;
-        public enum Autostart_Types
-        {
-            NONE = 0,
-            VIA_METRONOME,
-        }
 
         public static event EventHandler OnConfigReload;
         public static void ReloadConfig()
