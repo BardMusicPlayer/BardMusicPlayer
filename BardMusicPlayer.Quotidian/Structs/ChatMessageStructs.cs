aaf975ef213b4c5e3a3c3bbba67ff82b36a20614
Meowchestra <42126400+Meowchestra@users.noreply.github.com>
2023-01-29 21:26:10 -0800

    Clean up chat types
    * Removed 'macro' type since it wasn't name clarified as using whatever the client had set as default.
    * Renamed group to party.
    * Added missing party option.
    * Capitalized to match in-game categories.

--- BardMusicPlayer.Quotidian/Structs/ChatMessageStructs.cs
+++ BardMusicPlayer.Quotidian/Structs/ChatMessageStructs.cs
@@ -8,18 +8,18 @@ public readonly struct ChatMessageChannelType
 {
     public static readonly ChatMessageChannelType None  = new("None",   0x0000, "");
     public static readonly ChatMessageChannelType Say   = new("Say",    0x000A, "/s");
-    public static readonly ChatMessageChannelType Shout = new("Shout",  0x000B, "/sh");
-    public static readonly ChatMessageChannelType Group = new("Group",  0x000E, "/p");
-    public static readonly ChatMessageChannelType FC    = new("FC",     0x0018, "/fc");
     public static readonly ChatMessageChannelType Yell  = new("Yell",   0x001E, "/y");
+    public static readonly ChatMessageChannelType Shout = new("Shout",  0x000B, "/sh");
+    public static readonly ChatMessageChannelType Party = new("Party",  0x000E, "/p");
+    public static readonly ChatMessageChannelType FC    = new("FC",     0x0018, "/fc");
     public static readonly IReadOnlyList<ChatMessageChannelType> All = new ReadOnlyCollection<ChatMessageChannelType>(new List<ChatMessageChannelType>
     {
         None,
         Say,
+        Yell,
         Shout,
-        Group,
-        FC,
-        Yell
+        Party,
+        FC
     });
 
     public string Name { get; }
