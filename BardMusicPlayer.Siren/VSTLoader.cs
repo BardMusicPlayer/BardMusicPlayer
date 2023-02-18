5507a24b97409e05a1d4b0767b0a3c5e5bd95142
Meowchestra <42126400+Meowchestra@users.noreply.github.com>
2023-02-18 15:07:21 -0800

    Siren Cleanup
    File-scope namespaced.

--- BardMusicPlayer.Siren/VSTLoader.cs
+++ BardMusicPlayer.Siren/VSTLoader.cs
@@ -11,75 +11,74 @@ using System.IO;
 using BardMusicPlayer.Quotidian.Structs;
 using System.Diagnostics;
 
-namespace BardMusicPlayer.Siren
+namespace BardMusicPlayer.Siren;
+
+internal static class VSTLoader
 {
-    internal static class VSTLoader
+    internal static async void UpdateAndLoadVST()
     {
-        internal static async void UpdateAndLoadVST()
+        try
         {
-            try
+            if (!Directory.Exists(BmpSiren.Instance._vstLocation)) Directory.CreateDirectory(BmpSiren.Instance._vstLocation);
+
+            if (File.Exists(BmpSiren.Instance._vstLocation + @"\version"))
             {
-                if (!Directory.Exists(BmpSiren.Instance._vstLocation)) Directory.CreateDirectory(BmpSiren.Instance._vstLocation);
+                using TextReader reader = File.OpenText(BmpSiren.Instance._vstLocation + @"\version");
+                _version = int.Parse(reader.ReadLine());
+            }
+            int version = int.Parse(await GetStringFromUrl(_baseURL + @"/version"));
 
-                if (File.Exists(BmpSiren.Instance._vstLocation + @"\version"))
+            if (version > _version)
+            {
+                foreach (Instrument instrument in Instrument.All)
                 {
-                    using TextReader reader = File.OpenText(BmpSiren.Instance._vstLocation + @"\version");
-                    _version = int.Parse(reader.ReadLine());
-                }
-                int version = int.Parse(await GetStringFromUrl(_baseURL + @"/version"));
-
-                if (version > _version)
-                {
-                    foreach (Instrument instrument in Instrument.All)
-                    {
-                        Debug.WriteLine("Downloading VST file " + _baseURL + @"/vst_" + @instrument.Name.ToLower() + @".sf2");
-                        using var sf2writer = new FileStream(BmpSiren.Instance._vstLocation + @"\vst_" + @instrument.Name.ToLower() + @".sf2", FileMode.Create, FileAccess.Write);
-                        byte[] sf2 = await GetFileFromUrl(_baseURL + @"/vst_" + @instrument.Name.ToLower() + @".sf2");
-                        sf2writer.Write(sf2, 0, sf2.Length);
-                    }
-
-                    _version = version;
-                    using var writer = new StreamWriter(BmpSiren.Instance._vstLocation + @"\version");
-                    writer.Write(_version.ToString());
+                    Debug.WriteLine("Downloading VST file " + _baseURL + @"/vst_" + @instrument.Name.ToLower() + @".sf2");
+                    using var sf2writer = new FileStream(BmpSiren.Instance._vstLocation + @"\vst_" + @instrument.Name.ToLower() + @".sf2", FileMode.Create, FileAccess.Write);
+                    byte[] sf2 = await GetFileFromUrl(_baseURL + @"/vst_" + @instrument.Name.ToLower() + @".sf2");
+                    sf2writer.Write(sf2, 0, sf2.Length);
                 }
 
-                foreach (Instrument instrument in Instrument.All) BmpSiren.Instance._player.LoadSoundFont(File.ReadAllBytes(BmpSiren.Instance._vstLocation + @"\vst_" + @instrument.Name.ToLower() + @".sf2"), true);
-                BmpSiren.Instance._vstDownloaded = true;
+                _version = version;
+                using var writer = new StreamWriter(BmpSiren.Instance._vstLocation + @"\version");
+                writer.Write(_version.ToString());
             }
-            catch (Exception)
+
+            foreach (Instrument instrument in Instrument.All) BmpSiren.Instance._player.LoadSoundFont(File.ReadAllBytes(BmpSiren.Instance._vstLocation + @"\vst_" + @instrument.Name.ToLower() + @".sf2"), true);
+            BmpSiren.Instance._vstDownloaded = true;
+        }
+        catch (Exception)
+        {
+            // TODO: Create a solution for failed vst downloading
+        }
+    }
+    private static readonly string _baseURL = "https://dl.bardmusicplayer.com/bmp/vst";
+    private static int _version = 0;
+
+    private static async Task<string> GetStringFromUrl(string url)
+    {
+        var httpClient = new HttpClient();
+        return await Policy
+            .Handle<HttpRequestException>()
+            .WaitAndRetryAsync(retryCount: 3, sleepDurationProvider: i => TimeSpan.FromMilliseconds(300))
+            .ExecuteAsync(async () =>
             {
-                // TODO: Create a solution for failed vst downloading
-            }
-        }
-        private static readonly string _baseURL = "https://dl.bardmusicplayer.com/bmp/vst";
-        private static int _version = 0;
+                using var httpResponse = await httpClient.GetAsync(url);
+                httpResponse.EnsureSuccessStatusCode();
+                return await httpResponse.Content.ReadAsStringAsync();
+            });
+    }
 
-        private static async Task<string> GetStringFromUrl(string url)
-        {
-            var httpClient = new HttpClient();
-            return await Policy
-                .Handle<HttpRequestException>()
-                .WaitAndRetryAsync(retryCount: 3, sleepDurationProvider: i => TimeSpan.FromMilliseconds(300))
-                .ExecuteAsync(async () =>
-                {
-                    using var httpResponse = await httpClient.GetAsync(url);
-                    httpResponse.EnsureSuccessStatusCode();
-                    return await httpResponse.Content.ReadAsStringAsync();
-                });
-        }
-
-        private static async Task<byte[]> GetFileFromUrl(string url)
-        {
-            var httpClient = new HttpClient();
-            return await Policy
-                .Handle<HttpRequestException>()
-                .WaitAndRetryAsync(retryCount: 3, sleepDurationProvider: i => TimeSpan.FromMilliseconds(300))
-                .ExecuteAsync(async () =>
-                {
-                    using var httpResponse = await httpClient.GetAsync(url);
-                    httpResponse.EnsureSuccessStatusCode();
-                    return await httpResponse.Content.ReadAsByteArrayAsync();
-                });
-        }
+    private static async Task<byte[]> GetFileFromUrl(string url)
+    {
+        var httpClient = new HttpClient();
+        return await Policy
+            .Handle<HttpRequestException>()
+            .WaitAndRetryAsync(retryCount: 3, sleepDurationProvider: i => TimeSpan.FromMilliseconds(300))
+            .ExecuteAsync(async () =>
+            {
+                using var httpResponse = await httpClient.GetAsync(url);
+                httpResponse.EnsureSuccessStatusCode();
+                return await httpResponse.Content.ReadAsByteArrayAsync();
+            });
     }
 }
